98fcc0186830a82d1de829a85123068d3044ef9c
Shlomi Shitrit <akunh113@gmail.com>
2024-05-13 13:04:49 +0300

    init project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <>
+            <iframe
+                src="https://example.com"
+                width="100%"
+                height="1000"
+                title="Starting Big"
+                loading="eager"
+            />
+        </>
+    );
+}
